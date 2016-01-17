236001b4f5cd7171f67a56174b2a632344add265
James Oldfield <hi@james-oldfield.co>
2016-01-17 17:41:39 +0000

    basic hough lines implementation

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,71 +1,45 @@
 #include "ofApp.h"
+#include <math.h>
 
-//--------------------------------------------------------------
-void ofApp::setup(){
+using namespace ofxCv;
+using namespace cv;
 
-}
+vector<Vec4i> lines; // Storing the Hough lines
+Mat threshBin, img; // cv-style binary image
 
-//--------------------------------------------------------------
-void ofApp::update(){
+void ofApp::setup() {
+  image.loadImage("image.jpg");
 
-}
+  thresh.allocate(ofGetWidth(), ofGetHeight(), OF_IMAGE_GRAYSCALE);
+  convertColor(image, thresh, CV_RGB2GRAY);
+  
+  // Apply the canny edge detector for better segmentation
+  Canny(image, thresh, 50, 200, 3);
+  thresh.update();
 
-//--------------------------------------------------------------
-void ofApp::draw(){
+  // Map the OF image data to a cv-style binary data
+  threshBin = toCv(thresh);
 
-}
+  // Apply the Hough Lines transform
+  HoughLinesP(threshBin, lines, 1, CV_PI/180, 50, 50, 10);
 
-//--------------------------------------------------------------
-void ofApp::keyPressed(int key){
+  img = toCv(image); // Convert OF image to CV bin representation
 
-}
-
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
+  for( size_t i = 0; i < lines.size(); i++ ) {
+    Vec4i l = lines[i];
+    cv::line( img, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]), Scalar(0, 0, 255), 3, CV_AA);
+  }
 
 }
 
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
+void ofApp::update() {}
 
+void ofApp::draw() {
+
+  image.update();
+  image.draw(0, 0);
 }
 
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
+void ofApp::mousePressed(int x, int y, int button) {}
 
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
-}
+void ofApp::keyPressed(int key) {}
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,24 +1,21 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxCv.h"
 
-class ofApp : public ofBaseApp{
+class ofApp : public ofBaseApp {
+public:
+  void setup();
+  void update();
+  void draw();
+  void mousePressed(int x, int y, int button);
+  void keyPressed(int key);
+  
+  ofImage image;
+  ofImage thresh;
+  float tVal;
 
-	public:
-		void setup();
-		void update();
-		void draw();
-
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
+  ofxCv::ContourFinder contourFinder;
+  ofxCv::TrackingColorMode tcm;
+  ofColor targetColor;
 };
