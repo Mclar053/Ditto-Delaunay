c770bf95ca41d7bba82bf7b779f3fc7f2560e12a
James Oldfield <hi@james-oldfield.co>
2016-01-18 17:13:15 +0000

    increasing max line gap length, less image-specific

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -8,7 +8,8 @@ vector<Vec4i> lines; // Storing the Hough lines
 Mat threshBin, img; // cv-style binary image
 
 void ofApp::setup() {
-  image.loadImage("http://www.tekuto.com/wp-content/themes/tekuto2nd/images/topmain/toruso01.jpg?=20151006");
+  // image.loadImage("http://www.tekuto.com/wp-content/themes/tekuto2nd/images/topmain/toruso01.jpg?=20151006");
+  image.loadImage("http://static.dezeen.com/uploads/2008/02/squareparabola03.jpg");
 
   thresh.allocate(ofGetWidth(), ofGetHeight(), OF_IMAGE_GRAYSCALE);
   convertColor(image, thresh, CV_RGB2GRAY);
@@ -21,12 +22,13 @@ void ofApp::setup() {
   threshBin = toCv(thresh);
 
   // Apply the Hough Lines transform
-  HoughLinesP(threshBin, lines, 1, CV_PI/180, 150, 25, 200);
+  HoughLinesP(threshBin, lines, 1, CV_PI/180, 100, 25, 300);
 
   img = toCv(image); // Convert OF image to CV bin representation
 
   for( size_t i = 0; i < lines.size(); i++ ) {
     Vec4i l = lines[i];
+    cout << l << endl;
     cv::line( img, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]), Scalar(255, 0, 0), 1, CV_AA);
   }
 }
