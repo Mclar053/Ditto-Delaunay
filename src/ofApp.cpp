21567dd0e911051126477221b4cd82e06e683dcc
James Oldfield <hi@james-oldfield.co>
2016-02-22 14:14:26 +0000

    fixing master branch

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,5 +1,6 @@
 #include "ofApp.h"
 #include "segment.h"
+#include "Shape.h"
 
 using namespace ofxCv;
 using namespace cv;
@@ -56,52 +57,8 @@ void ofApp::setup() {
   for(auto l1 : cartCoord) {
     for(auto l2 : cartCoord) {
       doSegsIntersect(l1, l2);
-
     }
   }
-}
-
-/** OLD CODE
-//--------------------------------------------------------------
-void ofApp::setup(){
-    
-    //Simple shape comparison test
-    Shape s = Shape();
-    Shape t = Shape();
-    float an1, an2, an3;
-    an1 =s.getAngle(0);
-    an2 =s.getAngle(1);
-    an3 =s.getAngle(2);
-    
-    float tot = an1+an2+an3;
-    
-    t.setVertex(2, 0, 90); //True
-//    t.setVertex(3, 60, 90); //False
-    
-    cout<<"s angles"<<endl;
-    for(int i=0; i<s.getVertexSize(); i++){
-        cout<<"Angle "<<i<<": "<<s.getAngle(i)<<endl;
-    }
-    
-    cout<<"---------------"<<endl<<"t angles"<<endl;
-    for(int i=0; i<t.getVertexSize(); i++){
-        cout<<"Angle "<<i<<": "<<t.getAngle(i)<<endl;
->>>>>>> shape-comparison
-    }
-//    cout<<"s angles:"<<s.getAngle(0)<<", "<<s.getAngle(1)<<", "<<s.getAngle(2)<<endl;
-//    cout<<"t angles:"<<t.getAngle(0)<<", "<<t.getAngle(1)<<", "<<t.getAngle(2)<<endl;
-    
-    
-    if(s.compareAngle(t)){
-        cout<<"True"<<endl;
-    } else{
-        cout<<"False"<<endl;
-    }
-}
- **/
-
-//--------------------------------------------------------------
-void ofApp::update(){
 
   /*
    * FOR DEBUG MODE ONLY
@@ -135,13 +92,13 @@ void ofApp::update(){
 
 }
 
-//--------------------------------------------------------------
-void ofApp::draw(){
-
+void ofApp::update() {
+  image.update();
 }
 
-//--------------------------------------------------------------
-void ofApp::keyPressed(int key){
+void ofApp::draw() {
+  image.draw(0, 0);
+
   // Draw the points of intersection
   // given a reasonable threshold
   for(auto const &pt : iPts) {
@@ -229,54 +186,11 @@ bool ofApp::doSegsIntersect(ofPolyline a, ofPolyline b) {
     
     return 1;
   }
-}
 
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
+  return 0; // No collision
 
 }
 
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
+void ofApp::mousePressed(int x, int y, int button) {}
 
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
-}
+void ofApp::keyPressed(int key) {}
